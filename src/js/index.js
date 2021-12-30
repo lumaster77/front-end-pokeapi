627ecf281b37569131bc82c8d4a90d883ec6e0f3
jose guadalupe luquin angel <jglajose54@hotmail.com>
2021-12-29 18:33:47 -0600

    lista de pokemones y busqueda finalizada

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,109 @@
 const API = "https://pokeapi.co/api/v2/";
 const POKEMONS = document.getElementById('pokemons');
-const height = document.getElementById('height')
-const weight = document.getElementById('weight')
-const hp = document.getElementById('hp')
-const attack = document.getElementById('attack')
-const speed = document.getElementById('speed')
-const defense = document.getElementById('defense')
-const abilities = document.getElementById('abilitys')
-const species = document.getElementById('species')
-
+const POKEMON = document.getElementById('pokemon');
+var cardsPokemon = ''
 
 function GetAllPokemons(){
-    fetch(`${API}pokemon/`)
+    cardsPokemon='';
+    const offset= ramdomPokemons()
+    fetch(`${API}pokemon/?offset=${offset}&limit=21`)
     .then((res)=>{
         res.json()
         .then((pokemons)=>{
             console.log(pokemons)
-            showPokemons(pokemons)
+            findAllPokemons(pokemons)
         })
     })
     
 }
 
 function showPokemon(pokemon){
-    let index = 0;
-    const cardPokemon = POKEMONS.querySelector('#cardPokemon');
-    const image = cardPokemon.getElementsByClassName('containerCardBodyImageSprites')[index];
-    const name = cardPokemon.getElementsByClassName('containerCardHeaderTitle')[index];
-    const id = cardPokemon.getElementsByClassName('containerCardHeaderNumber')[index];
-
-    image.setAttribute('src', pokemon.sprites.front_default);
-   
-    name.textContent = pokemon.name.toUpperCase();
-    id.textContent = pokemon.id;
-    height.textContent = `Height: ${pokemon.height} Kg`;
-    weight.textContent = `Weight: ${pokemon.weight} Cm`;
-    hp.textContent = `Hp: ${pokemon.stats[0].base_stat} `;
-    attack.textContent = `Attack: ${pokemon.stats[2].base_stat} `;
-    speed.textContent = `speed: ${pokemon.stats[5].base_stat} `;
-    defense.textContent = `defense: ${pokemon.stats[3].base_stat} `;
-   
-    abilitiesPokemons(pokemon);
-    typesPokemons(pokemon);
+    cardsPokemon=''
+    cardsPokemon+=`<div id="cardPokemon" class="containerCard">
+    <div class="containerCardHeader">
+        <div class="containerCardHeaderTitle">${pokemon.name}</div>
+        <div class="containerCardHeaderNumber">${pokemon.id}</div>
+    </div>
+    <div class="containerCardBody">
+        <div class="containerCardBodyImage">
+            <img class="containerCardBodyImageSprites" src="${pokemon.sprites.front_default}" alt="">
+        </div>
+        <div class="containerCardBodySpecies">
+            <div id="species">`;
+            typesPokemons(pokemon);
+    cardsPokemon+= `</div>
+        </div>
+    </div>
+    <div class="containerCardFooter">
+        <div class="continerCardFooterInfo">
+            <p id="stats">Estadisticas</p>
+            <div>
+                <p id="height">height: ${pokemon.height}</p>
+                <p id="weight">weight: ${pokemon.weight}</p>
+            </div>
+            <div>
+                <p id="hp">Hp: ${pokemon.stats[0].base_stat}</p>
+                <p id="attack">Attack: ${pokemon.stats[1].base_stat}</p>
+            </div>
+            <div>
+                <p id="speed">Speed: ${pokemon.stats[5].base_stat}</p>
+                <p id="defense">Defense: ${pokemon.stats[2].base_stat}</p>
+            </div>
+           
+        </div>
+        <div class="containerCardFooterAbility">
+            <p id="stats">habilidades</p>
+            <div id="abilitys">`
+            abilitiesPokemons(pokemon); 
+            cardsPokemon+=`    </div>
+        </div>
+    </div>
+   </div>`    
     
+    
+    POKEMON.innerHTML=cardsPokemon;
+    POKEMONS.innerHTML="";
 }
 
+function showPokemons(pokemons){
+    
+    console.log(pokemons)
+    cardsPokemon += `<div class="containerPokemons">
+        <div class="cardPokemon" onclick="getOnePokemon(${pokemons.id})">
+            <div> ${pokemons.name}</div>
+            <div><img src="${pokemons.sprites.front_default}"></div>
+        </div></div>`
+    
+    POKEMONS.innerHTML=cardsPokemon;
+}
+function findAllPokemons(pokemons){
+        
+    pokemons.results.forEach(pokemon => {
+        fetch(pokemon.url)
+        .then(res => res.json())
+        .then(dataPokemon => showPokemons(dataPokemon))
+    })   
+}
 function typesPokemons(pokemons){
+    
     pokemons.types.forEach(type =>{
-        let content = `<div>${type.type.name}</div>`;
-        species.insertAdjacentHTML("beforeend", content)
+        cardsPokemon += `<div>${type.type.name}</div>`;
+        
     })
+    
 }
 
 function abilitiesPokemons(pokemons){
     pokemons.abilities.forEach(ability => {
-        let content = `<div>${ability.ability.name}</div>`;
-        abilities.insertAdjacentHTML("beforeend", content)
+        cardsPokemon += `<div>${ability.ability.name}</div>`;
+        
     })
 }
 
 function ramdomPokemons(){
-    return Math.round(Math.random()*898);
+    return Math.round(Math.random()*877);
 }
+
 
 
 function getOnePokemon(id){
@@ -70,7 +112,6 @@ function getOnePokemon(id){
     .then((res)=>{
         res.json()
         .then((pokemons)=>{
-            console.log(pokemons)
             showPokemon(pokemons)
         })
     })
@@ -81,6 +122,15 @@ function findOnePokemon(){
     console.log(id)
     getOnePokemon(id)
 }
+function menu(){
 
-getOnePokemon(ramdomPokemons())
+
+}
+
+function team(){
+
+
+}
+
+GetAllPokemons();
 
